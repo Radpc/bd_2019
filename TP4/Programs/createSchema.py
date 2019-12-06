e68d35c63bca9f50ad51ac86725aa8aa04f30ba8
Radpc <mgm3@icomp.ufam.edu.br>
2019-12-06 14:17:06 -0400

    Few modifications

--- TP4/Programs/createSchema.py
+++ TP4/Programs/createSchema.py
@@ -1,4 +1,25 @@
 import sys
+import psycopg2
+
+# First argument is SV_ADDRESS
+# Second argument is SV_USER
+# Third argument is SV_PASSWORD
+# Fourth argument is BD_NAME
+
+filename = "schema.sql"
+
+# Check if there is 4 args
+if (len(sys.argv) != 5):
+    print("Invalid arguments!")
+    exit()
+
+SV_ADDRESS, SV_USER, SV_PASSWORD, BD_NAME = sys.argv[1:]
+
+# con = psycopg2.connect(host=SV_ADDRESS, database=BD_NAME,
+#                       user=SV_USER, password=SV_PASSWORD)
+
+#cur = con.cursor()
 
 
-print(sys.argv[0])
+with open(filename, "r") as f:
+    print(f.read())
